5c6134593045e95aa0d1444af041f5c77253bf44
karim <karimservin@outlook.es>
2023-11-20 23:25:29 -0300

    Se agregó confirmarPoliza.fxml, se disenio la ventana, y se agrego confirmarPolizaController, falta desarrollarlo e integrarlo todo

---
+++ src/main/java/com/example/aseguradora/ConfirmarPolizaController.java
@@ -0,0 +1,18 @@
+package com.example.aseguradora;
+
+import javafx.fxml.FXML;
+
+public class ConfirmarPolizaController {
+
+    @FXML
+    private void cancelarAction() {
+        // Acciones al hacer clic en Cancelar
+        System.out.println("Operación cancelada");
+    }
+
+    @FXML
+    private void generarPolizaAction() {
+        // Lógica para generar la póliza
+        System.out.println("Generando la póliza...");
+    }
+}
